17fa7826cb6f91b652c9ca325dc63d48e32a293a
rohan <rsureshkumar@empyra.com>
2019-02-20 21:36:45 +0530

    Remove unnecessary code

--- Job Crawlers/xmljobfeed.py
+++ Job Crawlers/xmljobfeed.py
@@ -9,7 +9,6 @@ from xml.etree.ElementTree import ElementTree
 def read(self, url, tenantid, num_jobs=None):
     '''
     This function crawls and creates a dictionary of jobs from any xml job feed
-    You can view the jobs on their careers page: https://caesars.taleo.net/careersection/czr_ext_cs/jobsearch.ftl?lang=en&portal=8100120159
 
     Args:
         num_jobs: Number of jobs you want to parse
@@ -18,10 +17,8 @@ def read(self, url, tenantid, num_jobs=None):
         A dictionary of all the jobs with the job id as the key
     '''
     response = requests.get(url, verify=False)
-    #parser = XmlParser(response.text)
     tree = ElementTree()
     root = tree.parse("assets.xml")
-    #data = Data({'site' : 'grassroots'})
 
     items = [item for item in parser.get_elements(parser.dom, 'entry')]
     if (num_jobs is not None):
@@ -42,14 +39,8 @@ def read(self, url, tenantid, num_jobs=None):
                     job['Location'] = parser.get_attribute(category_e, 'term')
                 if (re.search('job_type', scheme)):
                     job_type_parsed = parser.get_attribute(category_e, 'term')
-                    job['JobType'] = ''
-                    job['JobType'] = 'Full-Time' if job_type_parsed == 'Full Time' else job['JobType']
-                    job['JobType'] = 'Part-Time' if job_type_parsed == 'Part Time' else job['JobType']
-                    job['JobType'] = 'Temporary' if job_type_parsed == 'Temporary' else job['JobType']
             links_e = parser.get_elements(item_e, 'link')
             link_e = next(iter(links_e))
             job['ExternalUrl'] = parser.get_attribute(link_e, 'href')
-            job['IsNew'] = True
             link_e = next(iter(links_e))
-            data.JOBS[job.get('ExternalId', '')] = job
-    return [job for job in data.JOBS.values()]
+    return jobs
